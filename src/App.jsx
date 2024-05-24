b1ac5159b3cf0806bc40e88c1c6e6ff9adb8f670
voja <voja@gmail.com>
2024-05-24 10:13:40 +0200

    04 Card refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import ButtonContainer from "./ButtonContainer";
 import Card from "./Card";
-import CountButtons from "./CountButtons";
-import Counter from "./Counter";
-import ResetButton from "./ResetButton";
-import Title from "./Title";
 
 function App() {
     return (
         <div className="container">
             <div className="row">
                 <div className="col-6 mx-auto mt-3">
-                    <Card>
-                        <Title />
-                        <Counter />
-                        <ResetButton />
-                        <ButtonContainer>
-                            <CountButtons />
-                        </ButtonContainer>
-                    </Card>
+                    <Card />
                 </div>
             </div>
         </div>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,20 @@
-function Card({ children }) {
+import CountButtons from "./CountButtons";
+import Counter from "./Counter";
+import ResetButton from "./ResetButton";
+import Title from "./Title";
+import ButtonContainer from "./ButtonContainer";
+
+function Card() {
     return (
         <div className="card">
-            <div className="card-body">{children}</div>
+            <div className="card-body">
+                <Title />
+                <Counter />
+                <ResetButton />
+                <ButtonContainer>
+                    <CountButtons />
+                </ButtonContainer>
+            </div>
         </div>
     );
 }
